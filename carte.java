1212132be5dbc271dee847cb04f15fe254883dff
mkhatiri <mohammed.khatiri@atos.net>
2022-05-11 14:06:31 +0200

    test changement

--- carte.java
+++ carte.java
@@ -2,7 +2,7 @@
 class Carte{
 
     public static void main(String[] args){
-        System.out.println("Hello, Je gere les Carte V1");
+        System.out.println("Hello, Je gere les Carte V2");
     }
 
 }
